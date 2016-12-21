1a95bb44e79a53a3beedfdd66acd464cd52193f4
Gun Gun Febrianza <gungunfebrianza@gmail.com>
2016-12-22 00:20:08 +0700

    add

--- demo/dashboard/utils.js
+++ demo/dashboard/utils.js
@@ -3,7 +3,7 @@ window.chartColors = {
 	orange: 'rgb(255, 159, 64)',
 	yellow: 'rgb(255, 205, 86)',
 	green: 'rgb(75, 192, 192)',
-	blue: 'rgb(54, 162, 235)',
+	blue: 'rgb(55, 162, 235)',
 	purple: 'rgb(153, 102, 255)',
 	grey: 'rgb(231,233,237)'
 };
